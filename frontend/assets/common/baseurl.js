cd9b273c2e4fdb1d0196a1c05d9883bd617f5f28
RemdelBarela <remdeleiron.barela@tup.edu.ph>
2024-03-22 14:48:45 +0800

    fix

--- frontend/assets/common/baseurl.js
+++ frontend/assets/common/baseurl.js
@@ -3,9 +3,9 @@ import { Platform } from 'react-native'
 
 let baseURL = '';
 
-{Platform.OS == 'ios'
-? baseURL = 'http://192.168.68.111:4000/api/v1/'
-: baseURL = 'http://192.168.68.100:4000/api/v1/'
+{Platform.OS == 'android'
+? baseURL = 'http://192.168.100.38:4000/api/v1/'
+: baseURL = 'http://192.168.100.50:4000/api/v1/'
 }
 
 export default baseURL;
