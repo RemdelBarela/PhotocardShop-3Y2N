cdb089b5aa4e6312cbe5bda174a1163e16d2c609
JhaleAustin <atheahisuan36@gmail.com>
2024-03-31 16:38:07 +0800

    dffd

--- frontend/Screens/Admin/Users.js
+++ frontend/Screens/Admin/Users.js
@@ -2,14 +2,13 @@ import React, { useState, useCallback } from "react";
 import {
     View,
     Text,
-    FlatList,
     ActivityIndicator,
     StyleSheet,
     Dimensions,
-    RefreshControl,
     Image,
     Modal,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView,
 } from "react-native";
 import { Box } from "native-base";
 import { DataTable, Searchbar } from "react-native-paper";
@@ -24,34 +23,40 @@ import baseURL from "../../assets/common/baseurl"
 import EasyButton from "../../Shared/StyledComponents/EasyButton";
 
 const Users = (props) => {
-    const [userList, setUserList] = useState([]);
-    const [userFilter, setUserFilter] = useState([]);
+
+    const [photoList, setPhotoList] = useState([]);
+    const [photoFilter, setPhotoFilter] = useState([]);
     const [loading, setLoading] = useState(true);
     const [token, setToken] = useState();
     const [refreshing, setRefreshing] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
-    const [selectedUser, setSelectedUser] = useState(null);
+    const [selectedPhoto, setSelectedPhoto] = useState(null);
+    const [imageModalVisible, setImageModalVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
     const navigation = useNavigation()
 
-    const searchUser = (text) => {
+
+    const searchPhoto = (text) => {
         if (text === "") {
-            setUserFilter(userList)
+            setPhotoFilter(photoList)
         }
-        setUserFilter(
-            userList.filter((i) =>
+        setPhotoFilter(
+            photoList.filter((i) =>
                 i.name.toLowerCase().includes(text.toLowerCase())
             )
         )
     }
 
-    const deleteUser = (id) => {
+    const deletePhoto = (id) => {
         axios
             .delete(`${baseURL}users/${id}`, {
                 headers: { Authorization: `Bearer ${token}` },
             })
             .then((res) => {
-                const users = userFilter.filter((item) => item.id !== id)
-                setUserFilter(users)
+                const users = photoFilter.filter((item) => item.id !== id)
+                setPhotoFilter(users)
+
+                onRefresh()
             })
             .catch((error) => console.log(error));
     }
@@ -63,13 +68,14 @@ const Users = (props) => {
                 .get(`${baseURL}users`)
                 .then((res) => {
                     // console.log(res.data)
-                    setUserList(res.data);
-                    setUserFilter(res.data);
+                    setPhotoList(res.data);
+                    setPhotoFilter(res.data);
                     setLoading(false);
                 })
             setRefreshing(false);
         }, 2000);
     }, []);
+
     useFocusEffect(
         useCallback(
             () => {
@@ -83,14 +89,14 @@ const Users = (props) => {
                     .get(`${baseURL}users`)
                     .then((res) => {
                         console.log(res.data)
-                        setUserList(res.data);
-                        setUserFilter(res.data);
+                        setPhotoList(res.data);
+                        setPhotoFilter(res.data);
                         setLoading(false);
                     })
 
                 return () => {
-                    setUserList();
-                    setUserFilter();
+                    setPhotoList();
+                    setPhotoFilter();
                     setLoading(true);
                 }
             },
@@ -98,28 +104,73 @@ const Users = (props) => {
         )
     )
 
-    const handleRowPress = (user) => {
-        setSelectedUser(user);
+    const handleRowPress = (photo) => {
+        setSelectedPhoto(photo);
         setModalVisible(true);
     };
+
+    const handleImagePress = (imageUrl) => {
+        setSelectedImage(imageUrl);
+        setImageModalVisible(true);
+    };
+    const renderGallery = () => {
+        // Check if selectedPhoto is defined and has image property
+        if (selectedPhoto && selectedPhoto.image) {
+            return (
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                    {/* Check if image is an array */}
+                    {Array.isArray(selectedPhoto.image) ? (
+                        // Map over the image array
+                        selectedPhoto.image.map((imageUrl, idx) => (
+                            <TouchableOpacity key={idx} onPress={() => handleImagePress(imageUrl)}>
+                                <Image
+                                    source={{
+                                        uri: imageUrl || null, // Ensure imageUrl is not undefined
+                                    }}
+                                    resizeMode="cover"
+                                    style={{ width: width, height: width / 2, marginVertical: 5 }}
+                                    onError={() => console.log('Error loading image')}
+                                />
+                            </TouchableOpacity>
+                        ))
+                    ) : (
+                        // Render a single image if not an array
+                        <TouchableOpacity onPress={() => handleImagePress(selectedPhoto.image)}>
+                            <Image
+                                source={{
+                                    uri: selectedPhoto.image || null, // Ensure imageUrl is not undefined
+                                }}
+                                resizeMode="cover"
+                                style={{ width: width, height: width / 2, marginVertical: 5 }}
+                                onError={() => console.log('Error loading image')}
+                            />
+                        </TouchableOpacity>
+                    )}
+                </ScrollView>
+            );
+        } else {
+            return <Text>No images available for this photo.</Text>;
+        }
+    };
+    
+    
 
     return (
         <Box flex={1}>
             <View style={styles.buttonContainer}>
-                
                 <EasyButton
                     secondary
                     medium
-                    onPress={() => navigation.navigate("Register")}
+                    onPress={() => navigation.navigate("PhotoForm")}
                 >
                     <Icon name="plus" size={18} color="white" />
-                    <Text style={styles.buttonText}>ADD USER</Text>
+                    <Text style={styles.buttonText}> ADD</Text>
                 </EasyButton>
             </View>
 
             <Searchbar width="80%"
-                placeholder="Search User"
-                onChangeText={(text) => searchUser(text)}
+                placeholder="Search Photo Name"
+                onChangeText={(text) => searchPhoto(text)}
             />
 
             <Modal
@@ -146,33 +197,70 @@ const Users = (props) => {
                         >
                             <Icon name="close" size={20} />
                         </TouchableOpacity>
-                        {selectedUser && (
+                        {selectedPhoto && (
                             <>
-                                <Text>{selectedUser.name}</Text>
+                                <Text>{selectedPhoto.name}</Text>
+                                {renderGallery()}
                                 <EasyButton
                                     medium
                                     secondary
                                     onPress={() => {
-                                        navigation.navigate("Register", { user: selectedUser });
+                                        navigation.navigate("PhotoForm", { item: selectedPhoto });
                                         setModalVisible(false);
                                     }}
                                     title="Edit"
                                 >
-                                    <Text style={styles.textStyle}>Edit</Text>
+                                    <Text style={styles.textStyle}>EDIT</Text>
                                 </EasyButton>
                                 <EasyButton
                                     medium
                                     danger
                                     onPress={() => {
-                                        deleteUser(selectedUser.id);
+                                        deletePhoto(selectedPhoto.id);
                                         setModalVisible(false);
                                     }}
                                     title="Delete"
                                 >
-                                    <Text style={styles.textStyle}>Delete</Text>
+                                    <Text style={styles.textStyle}>DELETE</Text>
                                 </EasyButton>
                             </>
                         )}
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={imageModalVisible}
+                onRequestClose={() => {
+                    setImageModalVisible(false)
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TouchableOpacity
+                            underlayColor="#E8E8E8"
+                            onPress={() => {
+                                setImageModalVisible(false)
+                            }}
+                            style={{
+                                alignSelf: "flex-end",
+                                position: "absolute",
+                                top: 5,
+                                right: 10
+                            }}
+                        >
+                            <Icon name="close" size={20} />
+                        </TouchableOpacity>
+                        <Image
+                            source={{
+                                uri: selectedImage ? selectedImage : null,
+                            }}
+                            resizeMode="contain"
+                            style={{ width: width - 40, height: height / 2 }}
+                            onError={() => console.log('Error loading image')}
+                        />
                     </View>
                 </View>
             </Modal>
@@ -183,117 +271,88 @@ const Users = (props) => {
                 </View>
             ) : (
                 <DataTable>
-                    <DataTable.Header>
-                        <DataTable.Title>NAME</DataTable.Title>
-                        <DataTable.Title>EMAIL</DataTable.Title>
-                        <DataTable.Title>PASSWORD</DataTable.Title>
-                        <DataTable.Title>PHONE</DataTable.Title>
-                        {/* <DataTable.Title>ADDRESS</DataTable.Title> */}
-                        <DataTable.Title>ROLE</DataTable.Title>
-                        <DataTable.Title>IMAGES</DataTable.Title>
-                    </DataTable.Header>
-                    {userFilter.map((item, index) => (
+                    <DataTable.Header style={{ backgroundColor: 'black' }}>
+                        <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }} ><Text style={{ color: 'white' }}>NAME</Text></DataTable.Title>
+                      
+                 
+                             <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }}><Text  style={{ color: 'white' }}>EMAIL</Text></DataTable.Title>
+                            <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }}><Text  style={{ color: 'white' }}>PASSWORD</Text></DataTable.Title>
+                          <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: 'white' }}>PHONE</Text></DataTable.Title>
+                 
+                          <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }}><Text  style={{ color: 'white' }}>ROLE</Text></DataTable.Title>
+                          <DataTable.Title style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: 'white' }}>VIEW</Text></DataTable.Title>
+                 
+                      
+                         </DataTable.Header>
+                    {photoFilter.map((item, index) => (
                         <TouchableOpacity
                             key={index}
                             onPress={() => handleRowPress(item)}
-                            style={[styles.container, {
-                                backgroundColor: index % 2 == 0 ? "white" : "gainsboro"
-                            }]}
-                            >
-                                <DataTable.Row>
-                                    <DataTable.Cell>{item.name}</DataTable.Cell>
-                                    <DataTable.Cell>{item.email}</DataTable.Cell>
-                                    <DataTable.Cell>{item.password}</DataTable.Cell>
-                                    <DataTable.Cell>{item.phone}</DataTable.Cell>
-                                    {/* <DataTable.Cell>{item.address}</DataTable.Cell> */}
-                                    <DataTable.Cell>{item.isAdmin}</DataTable.Cell>
-                                    <DataTable.Cell>
-    {Array.isArray(item.image) ? (
-        item.image.map((imageUrl, idx) => (
-            <Image
-                key={idx}
-                source={{
-                    uri: imageUrl ? imageUrl : null
-                }}
-                resizeMode="contain"
-                style={styles.image}
-                onError={() => console.log("Error loading image")}
-            />
-        ))
-    ) : (
-        <Image
-            source={{
-                uri: item.image ? item.image : null
-            }}
-            resizeMode="contain"
-            style={styles.image}
-            onError={() => console.log("Error loading image")}
-        />
-    )}
-</DataTable.Cell>
-                                </DataTable.Row>
-                            </TouchableOpacity>
-                        ))}
-                    </DataTable>
-                )}
-            </Box>
-        );
-    }
-    
-    const styles = StyleSheet.create({
-        listHeader: {
-            flexDirection: 'row',
-            padding: 5,
-            backgroundColor: 'gainsboro'
+                            style={{
+                                backgroundColor: index % 2 === 0 ? 'lightgray' : 'gainsboro',
+                            }}>
+                            <DataTable.Row>
+                                <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>{item.name}</DataTable.Cell>
+                                    <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>{item.email}</DataTable.Cell>
+                                    <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>{item.password}</DataTable.Cell>
+                                  
+                                    <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>{item.phone}</DataTable.Cell>
+                                    <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>{item.isAdmin}</DataTable.Cell>
+                                  
+                           
+                               
+                                <DataTable.Cell style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                    <Text style={{ textDecorationLine: 'underline' }}>View</Text>
+                                </DataTable.Cell>
+                            </DataTable.Row>
+                        </TouchableOpacity>
+                    ))}
+                </DataTable>
+            )}
+        </Box>
+    );
+}
+
+const styles = StyleSheet.create({
+    spinner: {
+        height: height / 2,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    container: {
+        marginBottom: 160,
+        backgroundColor: 'white'
+    },
+    buttonContainer: {
+        margin: 20,
+        alignSelf: 'center',
+        flexDirection: 'row'
+    },
+    buttonText: {
+        marginLeft: 4,
+        color: 'white'
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
         },
-        headerItem: {
-            margin: 3,
-            width: width / 6
-        },
-        spinner: {
-            height: height / 2,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        container: {
-            marginBottom: 160,
-            backgroundColor: 'white'
-        },
-        buttonContainer: {
-            margin: 20,
-            alignSelf: 'center',
-            flexDirection: 'row'
-        },
-        buttonText: {
-            marginLeft: 4,
-            color: 'white'
-        },
-        image: {
-            width: 50,
-            height: 50,
-            marginRight: 5,
-        },
-        modalView: {
-            margin: 20,
-            backgroundColor: "white",
-            borderRadius: 20,
-            padding: 35,
-            alignItems: "center",
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 2
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5
-        },
-        centeredView: {
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: 22
-        },
-    });
-    
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+});
+
 export default Users;
